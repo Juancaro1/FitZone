eb94c1a01a0199a5b55797da57cf9f20149a56cc
CapCriss <cristian.riquelme.2001@gmail.com>
2024-12-08 16:27:18 -0300

    fix modelo reseñas solucionado

--- aplicacion/src/main/java/com/proyecto/modelos/Resena.java
+++ aplicacion/src/main/java/com/proyecto/modelos/Resena.java
@@ -31,25 +31,7 @@ public class Resena {
         this.puntuacion = 0;
         this.comentario = "";
     }
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public int getPuntuacion() {
-        return puntuacion;
-    }
-    public void setPuntuacion(int puntuacion) {
-        this.puntuacion = puntuacion;
-    }
-    public String getComentario() {
-        return comentario;
-    }
-    public void setComentario(String comentario) {
-        this.comentario = comentario;
-    }
-    
-    
+
+
 
 }
